295a21e3ceeda8194ccf3975abc85449931457fc
Tyler Crumpton <tyler.crumpton@gmail.com>
2016-08-13 12:14:08 -0500

    Implement config-parser and tests.

--- dooblr/config.py
+++ dooblr/config.py
@@ -1,4 +1,44 @@
-from ConfigParser import ConfigParser
+from ConfigParser import SafeConfigParser
+import StringIO
+import logging
+import re
 
-def load(file):
-    pass
+class DooblrConfigError(Exception):
+    def __init__(self, *args,**kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class Config(object):
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        self._config = SafeConfigParser()
+        self.measurements = {}
+
+    def load(self, filepath):
+        with open(filepath, 'r') as config_file:
+            self._parse(config_file)
+
+    def _parse(self, config_text):
+        self._config.readfp(StringIO.StringIO(config_text))
+
+        for measurement in self._config.sections():
+            self.measurements[measurement] = {}
+            if not self._config.has_option(measurement, "fields"):
+                raise DooblrConfigError("Measurement {m} does not contain required option 'fields'".format(m=measurement))
+            else:
+                self.measurements[measurement]["fields"] = self._parse_option(self._config.get(measurement, "fields"))
+
+            if not self._config.has_option(measurement, "topics"):
+                raise DooblrConfigError("Measurement {m} does not contain required option 'topics'".format(m=measurement))
+            else:
+                self.measurements[measurement]["topics"] = self._parse_option(self._config.get(measurement, "topics"))
+
+            if not self._config.has_option(measurement, "tags"):
+                self._logger.info("Measurement {m} does not contain optional option 'tags'".format(m=measurement))
+            else:
+                self.measurements[measurement]["tags"] = self._parse_option(self._config.get(measurement, "tags"))
+
+    @staticmethod
+    def _parse_option(option_string):
+        return re.split(r'[\s,]+', option_string)
+
--- test/test_config.py
+++ test/test_config.py
@@ -1,7 +1,137 @@
 import unittest
+from dooblr.config import Config, DooblrConfigError
 
 
-class ExampleTestCase(unittest.TestCase):
+class ParseTestCase(unittest.TestCase):
 
-    def test_example(self):
-        self.assertTrue(True)
+    def test_missing_field_value_raises_error(self):
+        config = Config()
+        config_text = "\n".join((
+                       "[measurement]",
+                       "topics: ml256/topic/device",
+                       "        ml256/topic/otherdevice",
+                       "tags: supertag, awesometag"))
+        with self.assertRaises(DooblrConfigError):
+            config._parse(config_text)
+
+    def test_missing_topic_value_raises_error(self):
+        config = Config()
+        config_text = "\n".join((
+                       "[measurement]",
+                       "fields: coolfield, neatfield",
+                       "tags: supertag, awesometag"))
+        with self.assertRaises(DooblrConfigError):
+            config._parse(config_text)
+
+    def test_missing_tag_value_does_not_raise_error(self):
+        config = Config()
+        config_text = "\n".join((
+                       "[measurement]",
+                       "topics: ml256/topic/device",
+                       "        ml256/topic/otherdevice",
+                       "fields: coolfield,neatfield"))
+        try:
+            config._parse(config_text)
+        except DooblrConfigError as e:
+            self.fail("Missing tag config raised an exception! ({e})".format(e=e))
+
+    def test_valid_config_does_not_raise_error(self):
+        config = Config()
+        config_text = "\n".join((
+                       "[measurement]",
+                       "topics: ml256/topic/device",
+                       "        ml256/topic/otherdevice",
+                       "fields: coolfield, neatfield",
+                       "tags: supertag, awesometag"))
+        try:
+            config._parse(config_text)
+        except DooblrConfigError as e:
+            self.fail("Valid config raised an exception! ({e})".format(e=e))
+
+    def test_single_topic_is_parsed(self):
+        config = Config()
+        config_text = "\n".join((
+                       "[measurement]",
+                       "topics: ml256/topic/device",
+                       "fields: coolfield, neatfield",
+                       "tags: supertag, awesometag"))
+
+        config._parse(config_text)
+        self.assertEquals(config.measurements["measurement"]["topics"], ["ml256/topic/device"])
+
+    def test_multiple_topics_are_parsed(self):
+        config = Config()
+        config_text = "\n".join((
+                       "[measurement]",
+                       "topics: ml256/topic/device",
+                       "        ml256/topic/otherdevice",
+                       "fields: coolfield, neatfield",
+                       "tags: supertag, awesometag"))
+
+        config._parse(config_text)
+        self.assertEquals(config.measurements["measurement"]["topics"], ["ml256/topic/device", "ml256/topic/otherdevice"])
+
+    def test_single_field_is_parsed(self):
+        config = Config()
+        config_text = "\n".join((
+            "[measurement]",
+            "topics: ml256/topic/device",
+            "        ml256/topic/otherdevice",
+            "fields: coolfield",
+            "tags: supertag, awesometag"))
+
+        config._parse(config_text)
+        self.assertEquals(config.measurements["measurement"]["fields"], ["coolfield"])
+
+    def test_multiple_fields_are_parsed(self):
+        config = Config()
+        config_text = "\n".join((
+            "[measurement]",
+            "topics: ml256/topic/device",
+            "        ml256/topic/otherdevice",
+            "fields: coolfield, neatfield",
+            "tags: supertag, awesometag"))
+
+        config._parse(config_text)
+        self.assertEquals(config.measurements["measurement"]["fields"], ["coolfield", "neatfield"])
+
+    def test_single_tag_is_parsed(self):
+        config = Config()
+        config_text = "\n".join((
+                       "[measurement]",
+                       "topics: ml256/topic/device",
+                       "        ml256/topic/otherdevice",
+                       "fields: coolfield, neatfield",
+                       "tags: supertag"))
+
+        config._parse(config_text)
+        self.assertEquals(config.measurements["measurement"]["tags"], ["supertag"])
+
+    def test_multiple_tags_are_parsed(self):
+        config = Config()
+        config_text = "\n".join((
+                       "[measurement]",
+                       "topics: ml256/topic/device",
+                       "        ml256/topic/otherdevice",
+                       "fields: coolfield, neatfield",
+                       "tags: supertag, awesometag"))
+
+        config._parse(config_text)
+        self.assertEquals(config.measurements["measurement"]["tags"], ["supertag", "awesometag"])
+
+
+class ParseOptionTestCase(unittest.TestCase):
+    def test_multiple_options_newlines_are_parsed(self):
+        config = Config()
+        option_text = "foo\n        bar"
+        self.assertEquals(config._parse_option(option_text), ["foo", "bar"])
+
+    def test_multiple_options_commas_are_parsed(self):
+        config = Config()
+        option_text = "foo, bar"
+        self.assertEquals(config._parse_option(option_text), ["foo", "bar"])
+
+    def test_multiple_options_commas_and_newlines_are_parsed(self):
+        config = Config()
+        option_text = "foo\n        bar, bah"
+        self.assertEquals(config._parse_option(option_text), ["foo", "bar", "bah"])
