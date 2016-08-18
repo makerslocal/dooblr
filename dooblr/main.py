9315435d1a2ea9edd23e1473095e70b8fe89ccdb
Tyler Crumpton <tyler.crumpton@gmail.com>
2016-08-18 01:07:55 -0500

    Make main config that imports measurement configs.

--- dooblr/main.py
+++ dooblr/main.py
@@ -1,6 +1,7 @@
 import logging
 import config
 import mqttclient
+import os
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -8,18 +9,37 @@ logger = logging.getLogger(__name__)
 
 def main():
     logger.info("Starting dooblr")
-    cfg = config.Config()
-    cfg.load("config.ini")
+    main_cfg = config.MainConfig()
+    default_cfg_path = os.path.join(os.path.expanduser("~"), ".dooblr")
+    default_cfg_file = os.path.join(default_cfg_path, "dooblr.ini")
+    if not os.path.isdir(default_cfg_path):
+        os.makedirs(default_cfg_path)
+        os.makedirs(os.path.join(default_cfg_path, "measurements"))
+
+    try:
+        main_cfg.load(default_cfg_file)
+    except IOError as e:
+        logger.error("Can't open config file {file}: {reason}".format(file=default_cfg_file, reason=e))
+        raise
 
     def callback(message):
         logger.info(message)
+
     client = mqttclient.MqttClient(callback)
-    client.connect("test.mosquitto.org", port=1883)
-    for m in cfg.measurements:
+    logger.info("Connecting to MQTT broker: {host}:{port}".format(host=main_cfg.mqtt_host, port=main_cfg.mqtt_port))
+    client.connect(main_cfg.mqtt_host, port=main_cfg.mqtt_port)
+
+    measurement_cfg = config.MeasurementConfig()
+    for (dirpath, _, filenames) in os.walk(main_cfg.config_dir):
+        for filename in filenames:
+            logger.info("Loading measurements from {file}".format(file=os.path.join(dirpath, filename)))
+            measurement_cfg.load(os.path.join(dirpath, filename))
+
+    for m in measurement_cfg.measurements:
         client.register_measurement(m,
-                                    topics=cfg.measurements[m]["topics"],
-                                    fields=cfg.measurements[m]["fields"],
-                                    tags=cfg.measurements[m]["tags"])
+                                    topics=measurement_cfg.measurements[m]["topics"],
+                                    fields=measurement_cfg.measurements[m]["fields"],
+                                    tags=measurement_cfg.measurements[m]["tags"])
     while True:
         client.loop()
 
